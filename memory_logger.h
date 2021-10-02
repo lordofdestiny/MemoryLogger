43ad60b4ff639e08f0c00bb7bab7027b3f0353a7
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2021-10-02 22:32:27 +0200

    Add project files.

---
+++ memory_logger.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LOG_MEMORY 1
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif // !_CRT_SECURE_NO_WARNINGS
+
+#ifndef empty
+#define empty() do{}while(0)
+#endif
+
+#ifndef _MEMORY_LOGGER_
+#define _MEMORY_LOGGER_
+#define MAX_POINTER_COUNT 100000
+void memoryLoggerInit(FILE*);
+int memoryLoggerAtExitHook(int);
+void* m_malloc(size_t, char*);
+void* m_calloc(size_t, size_t, char*);
+void* m_realloc(void*, size_t);
+void m_free(void*);
+void (printMemoryLogs)();
+
+#if LOG_MEMORY
+#define malloc(size, name) m_malloc((size),(name))
+#define calloc(count, size, name) m_calloc((count),(size),(name))
+#define realloc(pointer, size) m_realloc((pointer),(size))
+#define free(pointer) m_free((pointer))
+#define printMemoryLogs() (printMemoryLogs)()
+#define atexit(fun) (atexit)((fun))
+#else
+#define malloc(size, name) (malloc)((size))
+#define calloc(count, size, name) (calloc)((count),(size))
+#define realloc(pointer,size) (realloc)((pointer),(size))
+#define free(pointer) (free)((pointer))
+#define printMemoryLogs() empty()
+#define atexit(fun) empty()
+#endif
+#endif
